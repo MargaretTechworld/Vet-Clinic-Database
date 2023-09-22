9db5ed4889dfaeab50558d9ad689e5afb844b55d
MargaretTechworld <margarettechworld@gmail.com>
2023-09-22 15:12:41 +0000

    Udated the inserted data

--- data.sql
+++ data.sql
@@ -16,7 +16,7 @@ INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_k
 (5, 'Charmander', '2020-02-8', 0, FALSE, -11),
 (6, 'Plantmon', '2021-11-15', 2, TRUE , -5.7),
 (7, 'Squirtle', '1993-04-02', 3, FALSE, -12.13),
-(8, 'Angemon', '2005-06-12', 0, TRUE, -45),
+(8, 'Angemon', '2005-06-12', 1, TRUE, -45),
 (9, 'Boarmon', '2005-07-07', 7, TRUE, 20.4),
 (10, 'Blossom', '1998-10-13', 3, TRUE, 17),
 (11, 'Ditto', '2022-05-14', 4, TRUE, 22);
